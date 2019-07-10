d06588ab424f47310e9a7a9142c6710a87f17818
Vinh Nguyen <nguyenv2@outlook.com>
2019-07-10 14:15:42 -0700

    change image

--- src/components/file.js
+++ src/components/file.js
@@ -5,23 +5,26 @@ export class ImageForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      api: `http://taskmaster-dev.us-west-2.elasticbeanstalk.com/tasks/${this.props.taskId}/images`
+      api: `http://taskmaster-dev.us-west-2.elasticbeanstalk.com/tasks/${this.props.taskId}/images`,
     }
   }
   render() {
     let image;
     if(this.props.taskUrl) {
+      let thumbnail = `https://taskmaster-thumbnail.s3-us-west-2.amazonaws.com/thumbnail-${this.props.taskUrl.split(".com/")[1]}`;
+
       image = 
       <div>
-        <img src={this.props.taskUrl} alt="Task" height="50px" width="50px"/>
+        <a href={this.props.taskUrl}>
+          <img src={thumbnail} alt="Task"/>
+        </a>
       </div>
     } else {
       image =   
         <form action={this.state.api} method="POST" encType="multipart/form-data">
           <input id="file" name="file" type="file" />
           <div id="upload">
-
-          <input type="submit" value="Upload"/>
+          <input type="submit" value="Upload" onClick={this.refresh}/>
           </div>
         </form> 
     }
